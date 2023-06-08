ffe575c55016152b09aa7a7d76eaf51a11b1a489
Artem Kuznetsov <arteemkzunetsov@gmail.com>
2023-06-09 01:43:08 +0700

    code refactoring

--- table_generator.py
+++ table_generator.py
@@ -7,17 +7,24 @@ import shutil
 
 
 def main():
+    """Точка входа"""
     categories = generate_rand_categories(1000, 8)
-    json = read_json('./in/config.json')
-    if json:
-        write_out_tables(json, categories)
+    json_file = read_json('./in/config.json')
+    if json_file:
+        write_out_tables(json_file, categories)
     else:
         print('Config error!')
 
 
-def generate_rand_categories(arr_len, str_len):
+def generate_rand_categories(arr_len, str_len) -> list:
+    """
+    Генерирует рандомное количество рандомных строк категорий
+    :param arr_len: максимальное количество категорий
+    :param str_len: длина строки категории
+    :return: список строк категорий
+    """
     categories = []
-    for i in range(0, arr_len):
+    for i in range(1, arr_len + 1):
         categories.append(
             ''.join(random.choices(string.ascii_letters, k=str_len))
         )
@@ -25,7 +32,12 @@ def generate_rand_categories(arr_len, str_len):
     return categories
 
 
-def read_json(path):
+def read_json(path) -> list | None:
+    """
+    Читает json-файл
+    :param path: путь к json-файлу
+    :return: список словарей или None
+    """
     try:
         with open(path) as file:
             return json.load(file)
@@ -34,6 +46,11 @@ def read_json(path):
 
 
 def write_csv(data, path):
+    """
+    Записывает csv-файл с полями category, count
+    :param data: список данных в формате {category, count}
+    :param path: путь к файлу
+    """
     with open(path, 'w', newline='') as csvfile:
         header = ['category', 'count']
         writer = csv.DictWriter(csvfile, fieldnames=header)
@@ -42,37 +59,60 @@ def write_csv(data, path):
             writer.writerow(row)
 
 
-def generate_csv(categories):
+def generate_csv(categories) -> list:
+    """
+    Генерирует csv-файл в формате {category, count}
+    :param categories: список категорий
+    :return: список в формате csv-файла
+    """
     rows = []
-    # рандомное количество строк - категорий
-    for i in range(1, random.randint(1, len(categories)) + 1):
+    for i in range(0, random.randint(1, len(categories))):
         rows.append({'category': categories[i], 'count': 1})
     return rows
 
 
 def create_empty_dir(path):
+    """
+    Создает пустую директорию, перезаписывая существующую
+    :param path: путь к директории
+    """
     if os.path.exists(path):
         shutil.rmtree(path)
     os.makedirs(path)
 
 
-def write_out_tables(json, categories):
+def create_tables_in_dir(categories, path):
+    """
+    Создает директорию с учетом вложенности и наполняет ее сгенерированными csv-таблицами
+    :param categories: список категорий
+    :param path: путь к директории
+    """
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    for i in range(1, random.randint(1, 6)):
+        random.shuffle(categories)
+        write_csv(
+            generate_csv(categories),
+            f"{path}/table_{i}.csv"
+        )
+
+
+def write_out_tables(json_file, categories):
+    """
+    Обходит значения во входном json-файле, обходит даты в значениях, для каждой даты вызывает create_tables_in_dir
+    :param json_file: прочитанный json-файл
+    :param categories: список категорий
+    """
     out_path = './out'
     create_empty_dir(out_path)
 
-    for value in json:
-        parent_path = f"{out_path}/{value['location']}/{value['source']}"
-        if not os.path.exists(parent_path):
-            os.makedirs(parent_path)
-            for date in value['dates']:
-                child_path = f"{parent_path}/{date}"
-                if not os.path.exists(child_path):
-                    os.mkdir(child_path)
+    for value in json_file:
+        for date in value['dates']:
+            create_tables_in_dir(
+                categories,
+                f"{out_path}/{value['location']}/{value['source']}/{date}"
+            )
 
-                    # рандомное количество таблиц от 1 до 5 включительно
-                    for i in range(1, random.randint(1, 6)):
-                        random.shuffle(categories)
-                        write_csv(
-                            generate_csv(categories),
-                            f"{child_path}/table_{i}.csv"
-                        )
+
+main()
--- table_uploader.py
+++ table_uploader.py
@@ -6,12 +6,17 @@ import db_conf
 
 
 def main():
+    """Точка входа"""
     csv_paths = search_csv_files()
     write_csv_to_table(csv_paths)
     update_counts_in_table()
 
 
-def search_csv_files():
+def search_csv_files() -> list:
+    """
+    Ищет в папке ./out все .csv-файлы
+    :return: список путей
+    """
     paths = []
     for root, dirs, files in os.walk('./out'):
         for file in files:
@@ -20,7 +25,12 @@ def search_csv_files():
     return paths
 
 
-def read_csv(path):
+def read_csv(path) -> list | None:
+    """
+    Читает .csv-файл и приводит все значения поля count к типу int
+    :param path: путь к csv-файлу
+    :return: список словарей или None
+    """
     try:
         with open(path, newline='') as csvfile:
             rows = list(csv.DictReader(csvfile))
@@ -31,17 +41,21 @@ def read_csv(path):
         return None
 
 
-def connect():
+def connect() -> psycopg2.connection | None:
+    """Подключается к базе данных PostgreSQL"""
     try:
-        return psycopg2.connect(dbname=db_conf.DATABASE,
-                                user=db_conf.USERNAME,
-                                password=db_conf.PASSWORD,
-                                host=db_conf.HOST)
+        return psycopg2.connect(
+            dbname=db_conf.DATABASE,
+            user=db_conf.USERNAME,
+            password=db_conf.PASSWORD,
+            host=db_conf.HOST
+        )
     except psycopg2.OperationalError:
         return None
 
 
-def select(conn, sql):
+def select(conn, sql) -> list:
+    """Делает SQL-запрос, возвращающий таблицу значений"""
     with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as curs:
         curs.execute(sql)
         data = curs.fetchall()
@@ -49,12 +63,14 @@ def select(conn, sql):
 
 
 def query(conn, sql, data=None):
+    """Делает SQL-запрос, который ничего не возвращает"""
     with conn.cursor() as curs:
         curs.execute(sql, data)
         conn.commit()
 
 
 def write_csv_to_table(paths):
+    """Создает в базе данных таблицу, поочередно читает csv-файлы, формирует запрос и выполняет запись в таблицу"""
     conn = connect()
     query(conn, 'CREATE TABLE IF NOT EXISTS csv (category VARCHAR(8), count INT)')
 
@@ -71,8 +87,15 @@ def write_csv_to_table(paths):
 
 
 def update_counts_in_table():
+    """
+    Подсчитывает количество одинаковых категорий в таблице,
+    для каждой category обновляет count в соответствие с подсчетами
+    """
     conn = connect()
     categories_count_table = select(conn, 'SELECT category, count(*) AS count FROM csv GROUP BY 1')
     for row in categories_count_table:
         query(conn, 'UPDATE csv SET count = %s WHERE category = %s', (row['count'], row['category']))
     conn.close()
+
+
+main()
